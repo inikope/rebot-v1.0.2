1d75f5b9c21b750d825961a405bba328ff7e74f6
inikope <59614495+inikope@users.noreply.github.com>
2020-01-21 02:48:33 +0700

    HLig-6

--- index.js
+++ index.js
@@ -127,12 +127,13 @@ app.get('/', (req, res) => {
         })
         Promise.all([p1]).then(function(values){
             const linkhl = values[0];
+            const linkhl2 = "\'"+values[0]+"\'"
             var link1;
             var link2;
         got(`https://api.storiesig.com/highlight/${linkhl}`).json().then(res => {
                 console.log(res.reels.items);
-                console.log(res.reels.$[linkhl]);
-                const base = res.reels.items;
+                console.log(res.reels.$[linkhl2]);
+                const base = res.reels.linkhl2;
                 base[number2].video_versions === undefined ? link1 = base[i].image_versions2.candidates[0].url : link1 = base[i].video_versions[0].url;
                 link2 = base[i].image_versions2.candidates[0].url;
                 return;
